52e1ec5c67d6e4c9d50ba5a6bfbda6c039c4e936
Dream <just2dr3am@gmail.com>
2020-11-26 00:29:44 +0200

    +Read test success

--- RPG Java/src/ItemDatabase/Item.java
+++ RPG Java/src/ItemDatabase/Item.java
@@ -1,23 +1,49 @@
 package ItemDatabase;
 
-public class Item {
+public class Item 
+{
 	public int id;
 	public String title;
 	public String description;
 	public int atk;
 
-	public Item() {
+	public Item() 
+	{
 	}
 
-	public Item(int id, String title, String description, int atk) {
+	public Item(int id, String title, String description, int atk) 
+	{
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.atk = atk;
 	}
 
-	public int getId() {
+	public int getId() 
+	{
 		return id;
+	}
+	
+	public String getTitle() 
+	{
+		return title;
+	}
+	
+	public String getDesc() 
+	{
+		return description;
+	}
+	
+	public int getAtk() 
+	{
+		return atk;
+	}
+	
+	@Override
+	public String toString()
+	{
+		System.out.println("Name: " + title + "\nAttack: " + atk + "\nDescription: " + description);
+		return "1";
 	}
 
 }
--- RPG Java/src/ItemDatabase/ItemList.java
+++ RPG Java/src/ItemDatabase/ItemList.java
@@ -1,43 +1,61 @@
 package ItemDatabase;
 
 import java.io.File; // Import the File class
-import java.io.FileInputStream;
 import java.io.FileNotFoundException; // Import this class to handle errors
-import java.io.IOException;
 import java.util.Scanner; // Import the Scanner class to read text files
+//import java.io.FileInputStream;
+//import java.io.IOException;
 
-public class ItemList extends Item {
-	public static void main(String[] args) {
+public class ItemList extends Item
+{
+	//public void itemData(int Line)
+	public static void main(String[] args) //delete this
+	{
 		Item data = new Item();
-		try {
+		try 
+		{
 			File Obj = new File("items.txt");
-			FileInputStream is = new FileInputStream(Obj);
-			
+			int Line = 0; //delete this
+			/*FileInputStream is = new FileInputStream(Obj);
+
 			byte[] byteArray = new byte[(int) Obj.length()];
-			
+
 			try {
 				is.read(byteArray);
 				is.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 			String byteData = new String(byteArray);
-			
+
 			String[] stringArray = byteData.split("\r\n");
-			
-			int count = stringArray.length/4;
-			
+
+			int totalLines = stringArray.length / 4; -> Count number of lines in file*/
+
 			Scanner reader = new Scanner(Obj);
-			while (reader.hasNextLine()) {
+			for (int i = 0; i < Line; i++) 
+			{
 				data.id = reader.nextInt();
+				String skip = reader.nextLine();
 				data.title = reader.nextLine();
 				data.description = reader.nextLine();
 				data.atk = reader.nextInt();
 			}
-			reader.close();	
 			
-		} catch (FileNotFoundException e) {
+			data = new Item();
+			data.id = reader.nextInt();
+			String skip = reader.nextLine();
+			data.title = reader.nextLine();
+			data.description = reader.nextLine();
+			data.atk = reader.nextInt();
+			
+			data.toString();
+			
+			reader.close();
+
+		} catch (FileNotFoundException e) 
+		{
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
